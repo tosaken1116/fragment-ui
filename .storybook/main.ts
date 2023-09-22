97e5f5d9b31f55766a13fcd68918b34f0a1dc3b7
tosaken1116 <nagato20021122@gmail.com>
2023-09-22 22:03:07 +0900

    doc: change storybook readme dir

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,11 @@ import type { StorybookConfig } from "@storybook/react-vite";
 import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
-    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    stories: [
+        "../src/**/*.mdx",
+        "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+        "../README.mdx",
+    ],
     addons: [
         "@storybook/addon-links",
         "@storybook/addon-essentials",
